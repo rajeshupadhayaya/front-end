f675c04e340cff2607877c9e4cee23541d4162bf
Rajesh <rajesh>
2021-10-10 22:43:06 +0530

    some css fix

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -6,7 +6,7 @@ import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
-  styleUrls: ['./dialog.component.scss']
+  styleUrls: ['./dialog.component.css']
 })
 export class DialogComponent implements OnInit {
   message:string;
@@ -15,7 +15,7 @@ export class DialogComponent implements OnInit {
   cancel: string;
   header: string;
 
-  constructor(public thisDialogRef: MatDialogRef<DialogComponent>, 
+  constructor(public thisDialogRef: MatDialogRef<DialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data) {
       this.message = data.msg,
       this.title = data.title,
